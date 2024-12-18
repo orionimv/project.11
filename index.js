554fb0bfdc925895ddf168609d13d9b89af00551
Maksym Iliev <orioniv30@gmail.com>
2024-12-18 12:31:52 +0300

    problem

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('Happy developing ✨')
+document.addEventListener("mousedown", (e) =>{
+    let body = document.querySelector("body");
+    let circle = document.querySelector("span");
+    let x = e.pageX;
+    let y = e.pageY;
+    document.querySelector("#cursor").style.left = x + 'px';
+    document.querySelector("#cursor").style.left = y + 'px';
+    circle.style.left = x + 'px';
+    circle.style.top = y + 'px';
+    let size = Math.random() * Math.floor(Math.random() * (150 - 10 + 1) + 10);
+    circle.style.width = 20 + size + 'px';
+    circle.style.height = 20 + size + 'px';
+    body.appendChild(circle);
+    setTimeout(()=>{
+        circle.style.opacity = 0;
+        setTimeout(()=>{
+            circle.remove();
+        }, 2500)
+    },2500)
+
+})
+
+document.querySelector("#cursor").setAttribute("data-after","🧼");
